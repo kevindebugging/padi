5a43c6790376de4c77b95cb6f71278de20c84d19
kevindebugging <kevindebugging@gmail.com>
2016-03-23 15:34:42 +0800

    finish news section

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
@@ -53,6 +53,7 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
